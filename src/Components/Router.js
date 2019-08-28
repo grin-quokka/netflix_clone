39eddc55f7584bb55614ed801f6f98a61d896ba3
grin-quokka <grin_quokka@gmail.com>
2019-08-28 13:45:32 +0900

    header 추가

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,15 +1,24 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
 import Home from "../Routes/Home";
 import TV from "../Routes/TV";
 import Search from "../Routes/Search";
 
 export default () => (
   <Router>
-    <>
+    {/* Switch -> 한번에 하나만 route 되도록 */}
+    <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/tv" exact component={TV} />
-      <Route path="/search" exact component={Search} />
-    </>
+      <Route path="/search" component={Search} />
+
+      {/* 일치하는 경로가 없으면 to에 지정된 경로로 가라 */}
+      <Redirect from="*" to="/" />
+    </Switch>
   </Router>
 );
